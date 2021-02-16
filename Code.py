8ee2eacd13964504aea7aeb83f5000a73fc3e1e7
Roman <61010440+Aettio@users.noreply.github.com>
2021-02-16 15:55:47 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -10,3 +10,12 @@ from sklearn import tree, metrics
 # Подключаем данные
 
 df = pd.read_csv("CleanedData.csv")
+
+#Удаляем целевой столбец:
+
+X = df.drop(['Survived'], axis=1)
+y = df['Survived']
+
+# Разбиваем выборку на тренировочную и тестовую:
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0) 
